37bdc30fdcc10e8d6237b3e0210cfb590f211b60
John Strong <strngj411@gmail.com>
2014-06-27 07:31:25 +0100

    updated jasmine spec

--- test/jasmine/spec/BugleSpec.js
+++ test/jasmine/spec/BugleSpec.js
@@ -1,20 +1,17 @@
 
 
-describe('Bugle suite', function() {
+describe('api suite', function() {
 
 	var bugle,
 
 	methods = {
-		'_constructor': function(a, b) {
-			this.a = a;
-			this.b = b;
-		}
+		'_constructor': function() { }
 	};
 
 	beforeEach(function() {
 		bugle = Bugle.extend;
 	});
-	
+
 
 	it("should contain extend member", function() {
 		expect(bugle).toBeDefined();
@@ -30,19 +27,52 @@ describe('Bugle suite', function() {
 
 	it("should return an object that inherits Bugle methods", function() {
 		
-		var customBugle = bugle(methods)(1,2);
+		var customBugle = bugle(methods)();
 
 		expect(customBugle.pub).toBeDefined();
 		expect(customBugle.sub).toBeDefined();
 		expect(customBugle.unsub).toBeDefined();
 	});
 
+});
+
+describe('subscribe suite', function() {
+
+	var bugle;
+
+	beforeEach(function() {
+		
+		bugle = Bugle.extend({
+			
+			'_constructor': function(a, b) {
+				this.a = a;
+				this.b = b;
+			},
+
+			'getValues': function() {
+				this.pub('values', a, b);
+			}
+		});
+	});
+
 	it("should start with an empty state", function() {
 
-		var customBugle = bugle(methods)(1,2);
+		var customBugle = bugle(1,2);
 
 		expect(customBugle.oId).toBe(0);
 		expect(customBugle.topics).toEqual([]);
 	});
 
-})
+	it("can subscribe an anonymous function to a topic", function() {
+
+		var customBugle = bugle(1,2);
+		customBugle.sub('values', function() { });
+
+		var topicLen = customBugle.topics['values'].length,
+		fn = customBugle.topics['values'][0];
+
+		expect(topicLen).toBe(1);
+		expect(typeof fn).toBe('object');
+	});
+
+});
