79b9ca8e189731ea3cbfc1aaa5ec4ae67e95f73e
John Strong <strngj411@gmail.com>
2014-07-09 12:02:37 +0100

    added 0.4.0 min distribution file, removed ECMA 6 syntax, replace with 'bind' usage

--- test/custom/nodeTest.js
+++ test/custom/nodeTest.js
@@ -7,20 +7,23 @@ var custInstance = bugle.extend({
 	'_constructor': function() { }
 })();
 
-var oId1 = custInstance.sub('node', function(data) {
+var oId1 = custInstance.sub('node')
+
+oId1.receive(function(data) {
 	state = state.concat(data);
-
 	sys.puts(state.length === 4);
-
 });
 
-var oId2 = custInstance.sub('node', function(data) {
-	state = state.concat(data);
+var oId2 = custInstance.sub('node');
 
+oId2.receive(function(data) {
+	state = state.concat(data);
 	sys.puts(state.length === 8);
 });
 
 custInstance.pub('node', [1,2,3,4]);
 
-custInstance.unsub('node', oId1);
-custInstance.unsub('node', oId2);
+setTimeout(function() {
+	custInstance.unsub('node', oId1);
+	custInstance.unsub('node', oId2);	
+}, 1000);
